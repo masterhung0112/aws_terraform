5c197fc53eaa436f71e150e0ac146874e757a038
hung <hungbn0112@gmail.com>
2021-01-06 14:08:10 +0700

    Working ch3

--- ch3/gtm.tf
+++ ch3/gtm.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.hungknowtk.zone_id
-  name = "www"
+  name = "hungknow.tk"
   type = "A"
   ttl = "300"
   records = [
--- ch3/instances.tf
+++ ch3/instances.tf
@@ -1,11 +1,9 @@
 resource "aws_instance" "cheap_worker" {
-  ami = "ami-09d9bd006ceda097f"
+  ami = "ami-05bb13389b5d8cb2e"
   instance_type = "t2.micro"
-  availability_zone = "us-east-1c"
   associate_public_ip_address = true
   subnet_id = aws_subnet.mainvpc.id
   vpc_security_group_ids = [aws_default_security_group.mainvpc.id]
-  depends_on = [aws_internet_gateway.mainvpc_gw]
 
   tags = {
       Name = "CheapWorker"
@@ -20,14 +18,13 @@ output "ip" {
 
 # Oregon
 resource "aws_instance" "cheap_worker_west" {
-  ami = "ami-09d9bd006ceda097f"
+  ami = "ami-0433594864f028d1a"
   provider = aws.west
   instance_type = "t2.micro"
-  availability_zone = "us-west-2c"
+#   availability_zone = "us-west-2c"
   associate_public_ip_address = true
   subnet_id = aws_subnet.mainvpc_west.id
   vpc_security_group_ids = [aws_default_security_group.mainvpc_west.id]
-  depends_on = [aws_internet_gateway.mainvpc_west_gw]
 
   tags = {
       Name = "CheapWorker"
